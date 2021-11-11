236206875fcd0d8492f04eedb1ac718bb6d8bb32
Kyle Collie <kcollie@hotmail.com>
2021-11-11 10:39:08 -0500

    Add products service

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../shared/products.service';
 
 @Component({
   selector: 'app-inno-tech-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
+  constructor(private _productService: ProductsService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

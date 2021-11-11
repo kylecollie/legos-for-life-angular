3566a48dc2983b4d97a1321a4dad79c1c28df464
Kyle Collie <kcollie@hotmail.com>
2021-11-11 11:07:12 -0500

    Add getAll method on products service

--- src/app/products/shared/products.service.ts
+++ src/app/products/shared/products.service.ts
@@ -1,9 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductDto } from './product.dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
   constructor(private _http: HttpClient) {}
+
+  getAll(): Observable<ProductDto[]> {
+    return this._http.get<ProductDto[]>('https://localhost:5001/api/Product');
+  }
 }

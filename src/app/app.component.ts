a58d7d7b99af69880e8c9c69d99060925ba803c0
Kyle Collie <kcollie@hotmail.com>
2021-11-30 08:20:25 -0500

    Add BehaviorSubject for login status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/shared/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  jwt: string | null | undefined;
+  constructor(private _auth: AuthService) {
+    _auth.isLoggedIn$.subscribe((jwt) => {
+      this.jwt = jwt;
+    });
+  }
+
+  logout() {
+    this._auth.logout();
+  }
+}
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,32 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../environments/environment';
 import { LoginDto } from './login.dto';
 import { TokenDto } from './token.dto';
 
+const jwtToken = 'jwtToken';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  isLoggedIn$ = new BehaviorSubject<string | null>(this.getToken());
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
   login(loginDto: LoginDto): Observable<TokenDto> {
     return this._http
       .post<TokenDto>(environment.api + '/api/auth/login', loginDto)
       .pipe(
-        tap(token => {
+        tap((token) => {
           if (token && token.jwt) {
-            localStorage.setItem('jwtToken', token.jwt);
+            localStorage.setItem(jwtToken, token.jwt);
+            this.isLoggedIn$.next(token.jwt);
+          } else {
+            this.logout();
           }
         })
-      )
+      );
   }
 
-  getToken(): string | null
-  {
-    return localStorage.getItem('jwtToken');
+  logout() {
+    localStorage.removeItem(jwtToken);
+    this.isLoggedIn$.next(null);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(jwtToken);
   }
 }
